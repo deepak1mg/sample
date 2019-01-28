a5a5722c0f80e96e498cb8177a8702e21b692435
deepak1mg <deepak.mayal@1mg.com>
2019-01-28 14:42:38 +0530

    adding combo

---
+++ combo.py
@@ -0,0 +1,22 @@
+COURSES = {
+    "Python Basics": {"Python", "functions", "variables",
+                      "booleans", "integers", "floats",
+                      "arrays", "strings", "exceptions",
+                      "conditions", "input", "loops"},
+    "Java Basics": {"Java", "strings", "variables",
+                    "input", "exceptions", "integers",
+                    "booleans", "loops"},
+    "PHP Basics": {"PHP", "variables", "conditions",
+                   "integers", "floats", "strings",
+                   "booleans", "HTML"},
+    "Ruby Basics": {"Ruby", "strings", "floats",
+                    "integers", "conditions",
+                    "functions", "input"}
+}
+def covers(my_set):
+    ls=[]
+    for key,value in COURSES.items():
+        if len(value & my_set)==len(my_set):
+            ls.append(key)
+    return ls
+print(covers({"conditions", "input"}) )
